2186cfb6f23d3c74834c748abcbfaeab8c951788
Unknown <moriatandolab@gmail.com>
2020-12-12 19:51:59 +0900

    (12/12)first_submmit

---
+++ thesis/summarize_tex_src.py
@@ -0,0 +1,48 @@
+""" $ python3 summarize_tex_src.py """
+
+import sys
+
+# ここでソースファイル名を指定
+input_file_list =   ["abstract", 
+                    "introduction",
+                    "related_work",
+                    "basic_IQ",
+                    "segment_IQ",
+                    "switch",
+                    "eval",
+                    "summary",
+                    "publication",
+                    "acknowledgement"
+                  ]
+
+# 出力ファイル名
+output_file_name = "tex_src"
+
+# 区切り線
+separation = "+++++++++++++++++++++++++\n"
+
+def prosessing():
+    # 出力ファイルのオープン
+    output_file = open(output_file_name, "w")
+
+    # main を最初に開く
+    with open("main.tex", "r") as input_file:
+        text = input_file.read() # 取得
+        # 書き込み
+        output_file.write(separation + "main.tex\n" + separation)
+        output_file.write(text)
+        output_file.write("\n\n")
+
+    for name in input_file_list:
+        path = "src/" + name + ".tex" # src/filename.tex
+        with open(path, "r") as input_file:
+            text = input_file.read() # 取得
+            # 書き込み
+            output_file.write(separation + path + "\n" + separation)
+            output_file.write(text)
+            output_file.write("\n\n")
+
+    output_file.close() # ファイルクローズ
+
+if __name__ == "__main__":
+    prosessing()
